80938382fc713c5ffb61bedd3a1dd4cbb2b77509
Jared Nelson <jbn014@outlook.com>
2020-10-13 11:42:13 -0500

    update

--- main.js
+++ main.js
@@ -7,10 +7,19 @@ let recWidthInput = document.getElementById("rectangle-width");
 let sideLengthInput = document.getElementById("square-length");
 let radiusInput = document.getElementById("radius-length");
 let triHeightInput = document.getElementById("triangle-height");
-let shapeContainer = document.getElementById("shape-container")
+let shapeContainer = document.getElementById("shape-container");
+let sidePanel = document.getElementById("side-panel");
+let shapePanel = document.getElementById("shape-type");
+let shapeWidth = document.getElementById("shape-width");
+let shapeHeight = document.getElementById("shape-height");
+let shapeRadius = document.getElementById("shape-radius");
+let shapeArea = document.getElementById("shape-area");
+let shapePerimeter = document.getElementById("shape-perimeter");
 
 class Shape {
-  constructor() {}
+  constructor() {
+    
+  }
 }
 
 class Square extends Shape {
@@ -23,10 +32,13 @@ class Square extends Shape {
     this.newSquare.style.height = sideLengthValue + "px";
     this.newSquare.style.width = sideLengthValue + "px";
     this.newSquare.style.position = "absolute"
-    this.newSquare.style.top = Math.floor(Math.random()* Math.floor(600)) + "px"
-    this.newSquare.style.left = Math.floor(Math.random()* Math.floor(600)) + "px"
+    this.newSquare.style.top = Math.floor(Math.random()* Math.floor(600-sideLengthValue)) + "px"
+    this.newSquare.style.left = Math.floor(Math.random()* Math.floor(600-sideLengthValue)) + "px"
     shapeContainer.appendChild(this.newSquare);
     console.log(sideLengthValue);
+    this.newSquare.addEventListener ('dblclick', () => {
+      shapeContainer.removeChild(this.newSquare);
+    })
   }
 }
 
@@ -42,11 +54,14 @@ class Rectangle extends Shape {
     this.newRectangle.style.height = recHeightValue + "px";
     this.newRectangle.style.width = recWidthValue + "px";
     this.newRectangle.style.position = "absolute"
-    this.newRectangle.style.top = Math.floor(Math.random()* Math.floor(500)) + "px"
-    this.newRectangle.style.left = Math.floor(Math.random()* Math.floor(500)) + "px"
+    this.newRectangle.style.top = Math.floor(Math.random()* Math.floor(600-recHeightValue)) + "px"
+    this.newRectangle.style.left = Math.floor(Math.random()* Math.floor(600-recWidthValue)) + "px"
     shapeContainer.appendChild(this.newRectangle);
     console.log(recHeightValue);
     console.log(recWidthValue);
+    this.newRectangle.addEventListener ('dblclick', () => {
+      shapeContainer.removeChild(this.newRectangle);
+    })
   }
 }
 
@@ -61,10 +76,13 @@ class Circle extends Shape {
     this.newCircle.style.width = radiusValue + "px";
     this.newCircle.style.height = radiusValue + "px";
     this.newCircle.style.position = "absolute";
-    this.newCircle.style.top = Math.floor(Math.random()* Math.floor(500)) + "px"
-    this.newCircle.style.left = Math.floor(Math.random()* Math.floor(500)) + "px"
+    this.newCircle.style.top = Math.floor(Math.random()* Math.floor(600-radiusValue)) + "px"
+    this.newCircle.style.left = Math.floor(Math.random()* Math.floor(600-radiusValue)) + "px"
     shapeContainer.appendChild(this.newCircle);
     console.log(radiusValue);
+    this.newCircle.addEventListener ('dblclick', () => {
+      shapeContainer.removeChild(this.newCircle);
+    })
   }
 }
 
@@ -74,16 +92,19 @@ class Triangle extends Shape {
     this.triHeight = triHeight;
     let triHeightValue = parseInt(triHeightInput.value);
     console.log(triHeightValue);
-    this.newCircle = document.createElement("div");
-    this.newTriangle.style.backgroundColor = ("yellow");
-    this.newTriangle.style.border = triHeightValue + "px";
-    this.newTriangle.style.borderLeft = triHeightValue + "px";
+    this.newTriangle = document.createElement("div");
+    this.newTriangle.style.borderBottom = triHeightValue + "px solid yellow";
+    this.newTriangle.style.borderLeft = triHeightValue/2 + "px solid transparent";
+    this.newTriangle.style.borderRight = triHeightValue/2 + "px solid transparent";
     this.newTriangle.style.width = "0"
     this.newTriangle.style.height = "0"
     this.newTriangle.style.position = "absolute";
-    this.newTriangle.style.top = Math.floor(Math.random()* Math.floor(500)) + "px"
-    this.newTriangle.style.left = Math.floor(Math.random()* Math.floor(500)) + "px"
+    this.newTriangle.style.top = Math.floor(Math.random()* Math.floor(600-triHeightValue)) + "px"
+    this.newTriangle.style.left = Math.floor(Math.random()* Math.floor(600-triHeightValue)) + "px"
     shapeContainer.appendChild(this.newTriangle);
+    this.newTriangle.addEventListener ('dblclick', () => {
+      shapeContainer.removeChild(this.newTriangle);
+    })
   }
 }
 
